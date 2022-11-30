91aae05f0251ba18ef1579ca62e29825a99c9e77
Baker Kong <baker.kong@avepoint.com>
2022-11-30 13:16:07 +0800

    30

--- workbench/component/Preview.tsx
+++ workbench/component/Preview.tsx
@@ -1,13 +1,21 @@
+import { IconButton } from '@fluentui/react';
+import { Link } from 'react-router-dom';
 import styles from '../style/Preview.module.scss';
 
-export default function Preview(props){
+export default function Preview(props) {
 
     return <>
         <header className={styles.header}>
+            <Link to='/'>
+                <IconButton
+                    iconProps={{ iconName: 'Back' }}
+                    title="Back"
+                    ariaLabel="Back"
+                />
+            </Link>
             <div>webpart preview</div>
-            <div>webpart preview</div>
-            <div>webpart preview</div>
+            <IconButton iconProps={{ iconName: 'Settings' }} title="Settings" ariaLabel="Settings" />
         </header>
-        <section>main part</section>
+        <section className={styles.main}>main part</section>
     </>
 }
